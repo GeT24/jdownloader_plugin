d761bf5b74db1588ae688e7eb914e029601ff35d
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2020-03-23 18:38:38 +0100

    change api

--- GeT24Org.java
+++ GeT24Org.java
@@ -58,15 +58,17 @@ public class GeT24Org extends PluginForHost {
         // TODO: status
         final AccountInfo acc_info = new AccountInfo();
         this.br = newBrowser();
-        String response = br.postPage("http://dev.get24.org/api/login", "email=" + Encoding.urlEncode(account.getUser()) + "&passwd_sha256=" + JDHash.getSHA256(account.getPass()));
+        String response = br.postPage("https://get24.org/api/login", "email=" + Encoding.urlEncode(account.getUser()) + "&passwd_sha256=" + JDHash.getSHA256(account.getPass()));
         Long date_expire = TimeFormatter.getMilliSeconds(PluginJSonUtils.getJson(response, "date_expire"), "yyyy-MM-dd", Locale.ENGLISH);
         acc_info.setValidUntil(date_expire);
-        acc_info.setTrafficLeft(PluginJSonUtils.getJson(response, "transfer_left"));
-        acc_info.setTrafficMax(PluginJSonUtils.getJson(response, "transfer_max"));
+        long transfer_left = (long) (Float.parseFloat(PluginJSonUtils.getJson(response, "transfer_left")) * 1024 * 1024 * 1024);
+        acc_info.setTrafficLeft(transfer_left);
+        long transfer_max = (long) (Float.parseFloat(PluginJSonUtils.getJson(response, "transfer_max")) * 1024 * 1024 * 1024);
+        acc_info.setTrafficMax(transfer_max);
         account.setMaxSimultanDownloads(MAXSIM);
         account.setConcurrentUsePossible(true);
         // hosts list
-        response = br.getPage("http://dev.get24.org/api/hosts/enabled");
+        response = br.getPage("https://get24.org/api/hosts/enabled");
         ArrayList<String> supportedHosts = (ArrayList) JavaScriptEngineFactory.jsonToJavaObject(response);
         acc_info.setMultiHostSupport(this, supportedHosts);
         account.setType(AccountType.PREMIUM);
@@ -81,18 +83,50 @@ public class GeT24Org extends PluginForHost {
 
     public void handleMultiHost(final DownloadLink link, final Account account) throws Exception {
         this.br = newBrowser();
-        String post_data = "email=" + Encoding.urlEncode(account.getUser()) + "&passwd_sha256=" + JDHash.getSHA256(account.getPass()) + "&url=" + Encoding.urlEncode(link.getDownloadURL());
-        dl = jd.plugins.BrowserAdapter.openDownload(this.br, link, "http://dev.get24.org/api/download", post_data, false, 0);
-        if (dl.getConnection().getContentType().equalsIgnoreCase("application/json")) {
-            br.followConnection();
-            String response = br.toString();
-            if (PluginJSonUtils.getJson(response, "reason") == "no transfer") {
-                // TODO: wait to midnight 00:00 UTC
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_TEMP_DISABLE);
+        String post_data = "email=" + Encoding.urlEncode(account.getUser()) + "&passwd_sha256=" + JDHash.getSHA256(account.getPass()) + "&link=" + Encoding.urlEncode(link.getDownloadURL());
+        // "&url=" + Encoding.urlEncode(link.getDownloadURL());
+        String response = br.postPage("https://get24.org/api/debrid/geturl", post_data); // security
+        if (!Boolean.parseBoolean(PluginJSonUtils.getJson(response, "ok"))) {
+            String reason = PluginJSonUtils.getJson(response, "reason");
+            if (reason.equals("invalid credentials")) {
+                throw new PluginException(LinkStatus.ERROR_FATAL, "Wrong login or password");
+            } else if (reason.equals("user not activated")) {
+                throw new PluginException(LinkStatus.ERROR_FATAL, "Email not activated");
+            } else if (reason.equals("premium required")) {
+                throw new PluginException(LinkStatus.ERROR_PREMIUM, "premium required");
+            } else if (reason.equals("premiumX required")) {
+                throw new PluginException(LinkStatus.ERROR_PREMIUM, "premiumX required");
+            } else if (reason.equals("no transfer")) {
+                throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "daily limit exceeded");
+            } else if (reason.equals("host daily limit exceeded")) {
+                throw new PluginException(LinkStatus.ERROR_PREMIUM, "daily limit exceeded for this host"); // TODO: allow launching other
+                                                                                                           // links/hosts
+            } else if (reason.equals("file removed")) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            } else if (reason.equals("host not supported")) {
+                throw new PluginException(LinkStatus.ERROR_PREMIUM, "host not supported");
+            } else if (reason.equals("host disabled")) {
+                throw new PluginException(LinkStatus.ERROR_PREMIUM, "host disabled");
+            } else if (reason.equals("temporary error")) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "temporary error", 60 * 60 * 1000l); // we can try
+                                                                                                                         // another links or
+                                                                                                                         // hosts probably
+            } else if (reason.equals("unknown error")) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "unknown error", 60 * 60 * 1000l); // we can try another
+                                                                                                                       // hosts or maybe
+                                                                                                                       // even links
+            } else {
+                throw new PluginException(LinkStatus.ERROR_RETRY, "unknown error");
             }
-            // TODO: invalid credentials etc.
         }
+        String url = PluginJSonUtils.getJson(response, "url");
+        dl = jd.plugins.BrowserAdapter.openDownload(this.br, link, url);  // TODO: resume support etc
+        // TODO: validate status code
         dl.startDownload();
+        // link_hash
+        // url
+        // filename
+        // filesize
     }
 
     @Override
